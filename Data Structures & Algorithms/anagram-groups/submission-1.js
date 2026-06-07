e0defbc791c194553f7999d05793b8b41f7d9800
Arun <33630295+ArunNGun@users.noreply.github.com>
2026-06-07 17:02:39 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+
+    groupAnagrams(strs) {
+        const obj = {};
+
+        for (let str of strs) {
+            const key = [...str].sort().join(""); // sorted str = anagram key
+
+            if (!obj[key]) obj[key] = [];
+            obj[key].push(str);
+        }
+
+        return Object.values(obj);
+    }
+}
